cafb1537dd566a1ef77269b528a0bdbae783c0cb
Kamalov Tagaibek <tagai.nambavan@gmail.com>
2025-01-04 13:31:11 +0600

    feat: complete 43 of 83 tests

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  if (s2.length > s1.length) {
+    [s1, s2] = [s2, s1];
+  }
+  let count = 0;
+  for (let i = 0; i < s2.length; i += 1) {
+    if (s1.includes(s2.charAt(i))) {
+      s1 = s1.replace(s2.charAt(i), '');
+      count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const compare = (a, b) => {
+    if (a > b) {
+      return 1;
+    } else if (b > a) {
+      return -1;
+    }
+    return 0;
+  }
+  const sortedValues = arr.filter((value) => value !== -1).sort(compare);
+  const exceptionIndices = arr.reduce((acc, val, index) => {
+    if (val === -1) {
+      acc.push(index);
+    }
+    return acc;
+  }, []);
+  for (let i = 0; i < exceptionIndices.length; i += 1) {
+    sortedValues.splice(exceptionIndices[i], 0, -1);
+  }
+  return sortedValues;
 }
 
 module.exports = {
